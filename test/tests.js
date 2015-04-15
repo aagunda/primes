245dbed2d852548b720fd277de2888b3064a7cf8
Avi Gunda <aagunda@gmail.com>
2015-04-15 01:32:38 -0700

    fixed tabs

--- test/tests.js
+++ test/tests.js
@@ -11,83 +11,83 @@ function isPrime(n) {
 }
 
 describe('calcPrimes', function() {
-	it('should return buffer with entries from 0 to max', function(done) {
-		app.calcPrimes(100, function(err, result) {
-			assert.equal(result.length, 101);
-			done();
-		});
+  it('should return buffer with entries from 0 to max', function(done) {
+    app.calcPrimes(100, function(err, result) {
+      assert.equal(result.length, 101);
+      done();
+    });
 
-	});
+  });
 
-	it('should return a buffer with 1 at the position of prime numbers', function(done) {
-		app.calcPrimes(100, function(err, result) {
-			var correct = true;
-			for(var i = 0; i<result.length; i++) {
-				if (result[i] === 1) {
-					correct = correct && isPrime(i);
-				}
-			}
-			assert.equal(correct, true);
-			done();
-		});
+  it('should return a buffer with 1 at the position of prime numbers', function(done) {
+    app.calcPrimes(100, function(err, result) {
+      var correct = true;
+      for(var i = 0; i<result.length; i++) {
+        if (result[i] === 1) {
+          correct = correct && isPrime(i);
+        }
+      }
+      assert.equal(correct, true);
+      done();
+    });
 
-	});
+  });
 
-	it('should return a buffer with 0 at the position of composite numbers', function(done) {
-		app.calcPrimes(100, function(err, result) {
-			var correct = true;
-			for(var i = 0; i<result.length; i++) {
-				if (result[i] === 0) {
-					correct = correct && !isPrime(i);
-				}
-			}
-			assert.equal(correct, true);
-			done();
-		});
+  it('should return a buffer with 0 at the position of composite numbers', function(done) {
+    app.calcPrimes(100, function(err, result) {
+      var correct = true;
+      for(var i = 0; i<result.length; i++) {
+        if (result[i] === 0) {
+          correct = correct && !isPrime(i);
+        }
+      }
+      assert.equal(correct, true);
+      done();
+    });
 
-	});
+  });
 });
 
 
 describe('calcResults', function() {
 
-	var buf = new Buffer(10);
-	buf[0] = buf[1] = buf[4] = buf[6] = buf[8] = buf[9] = 0;
-	buf[2] = buf[3] = buf[5] = buf[7] = 1;
+  var buf = new Buffer(10);
+  buf[0] = buf[1] = buf[4] = buf[6] = buf[8] = buf[9] = 0;
+  buf[2] = buf[3] = buf[5] = buf[7] = 1;
 
-	it('should return an array of primes', function(done) {
-		app.calcResults(0, buf, function(err, count, sum, primes) {
-			var correct = true;
+  it('should return an array of primes', function(done) {
+    app.calcResults(0, buf, function(err, count, sum, primes) {
+      var correct = true;
 
-			for(var i = 0; i<primes.length; i++) {
-				correct = correct && isPrime(primes[i]);
-			}
+      for(var i = 0; i<primes.length; i++) {
+        correct = correct && isPrime(primes[i]);
+      }
 
-			assert.equal(correct, true);
-			done();
-		});
-	});
+      assert.equal(correct, true);
+      done();
+    });
+  });
 
-	it('should return the number of primes', function(done) {
-		app.calcResults(0, buf, function(err, count, sum, primes) {
-			var correct = true;
+  it('should return the number of primes', function(done) {
+    app.calcResults(0, buf, function(err, count, sum, primes) {
+      var correct = true;
 
-			assert.equal(count, primes.length);
-			done();
-		});
-	});
+      assert.equal(count, primes.length);
+      done();
+    });
+  });
 
-	it('should return the sum of the primes', function(done) {
-		app.calcResults(0, buf, function(err, count, sum, primes) {
-			var  sumcalc = 0;
+  it('should return the sum of the primes', function(done) {
+    app.calcResults(0, buf, function(err, count, sum, primes) {
+      var  sumcalc = 0;
 
-			for(var i = 0; i<primes.length; i++) {
-				sumcalc += primes[i];
-			}
+      for(var i = 0; i<primes.length; i++) {
+        sumcalc += primes[i];
+      }
 
-			assert.equal(sum, sumcalc);
-			done();
-		});
-	});
+      assert.equal(sum, sumcalc);
+      done();
+    });
+  });
 });
 
